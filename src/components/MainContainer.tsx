9edf46ec6e10adfc4d70cfe44eec62fed1b30af9
Neel Manglani <neel738@hotmail.com>
2022-01-08 00:23:57 -0400

    some init

---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export const MainContainer = () => {
+  return (
+    <React.Fragment>
+      <Button variant="contained" color="primary">
+        Start
+      </Button>
+    </React.Fragment>
+  );
+};
